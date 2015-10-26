6dad0952f488228519878ee84d69436526924daf
skchun <skchun@hawaii.edu>
2015-10-25 21:37:35 -1000

    Fix JSHint errors and add JSDocs.

--- simple-todos.js
+++ simple-todos.js
@@ -1,7 +1,13 @@
+/* exported Tasks */
+
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isServer) {
   // This code only runs on the server
+
+  /**
+   * Only publish tasks that aer public or belong to the current user
+   */
   Meteor.publish("tasks", function () {
     return Tasks.find({
       $or: [
@@ -17,6 +23,11 @@ if (Meteor.isClient) {
   Meteor.subscribe("tasks");
 
   Template.body.helpers({
+    /**
+     * Returns all tasks for a logged in user.
+     * Dos not display any hidden tasks.
+     * @returns {any.Cursor} The tasks.
+     */
     tasks: function () {
       if (Session.get("hideCompleted")) {
         // If hide completed is checked, filter tasks
@@ -26,15 +37,28 @@ if (Meteor.isClient) {
         return Tasks.find({}, {sort: {createdAt: -1}});
       }
     },
+    /**
+     * Returns true if the completed tasks should be hidden.
+     * @returns {any} Whether the completed tasks should be hidden.
+     */
     hideCompleted: function () {
       return Session.get("hideCompleted");
     },
+    /**
+     * Returns the count of tasks not yet checked completed.
+     * @returns {any} The number of tasks.
+     */
     incompleteCount: function () {
       return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
   Template.body.events({
+
+    /**
+     * Creates and saves a new task.
+     * @param event The browser event when the user clicks to create a new task.
+     */
     "submit .new-task": function (event) {
       // Prevent default browser form submit
       event.preventDefault();
@@ -48,25 +72,43 @@ if (Meteor.isClient) {
       // Clear form
       event.target.text.value = "";
     },
+    /**
+     * Set the state of the hideCompleted variable.
+     * @param event The event indicating whether the user checked or unchecked the box.
+     */
     "change .hide-completed input": function (event) {
       Session.set("hideCompleted", event.target.checked);
     }
   });
 
   Template.task.helpers({
+
+    /**
+     * Returns true if the current user is the owner of this task.
+     * @returns {boolean} True if the user owns this task.
+     */
     isOwner: function () {
       return this.owner === Meteor.userId();
     }
   });
 
   Template.task.events({
+    /**
+     * Set the checked property of the current task.
+     */
     "click .toggle-checked": function () {
       // Set the checked property to the opposite of its current value
       Meteor.call("setChecked", this._id, ! this.checked);
     },
+    /**
+     * Delete the current task.
+     */
     "click .delete": function () {
       Meteor.call("deleteTask", this._id);
     },
+    /**
+     * Set the current task to priate.
+     */
     "click .toggle-private": function () {
       Meteor.call("setPrivate", this._id, ! this.private);
     }
@@ -78,6 +120,10 @@ if (Meteor.isClient) {
 }
 
 Meteor.methods({
+  /**
+   * Creates a new task if the current user is logged in.
+   * @param text The task name.
+   */
   addTask: function (text) {
     // Make sure the user is logged in before inserting a task
     if (! Meteor.userId()) {
@@ -91,6 +137,11 @@ Meteor.methods({
       username: Meteor.user().username
     });
   },
+  /**
+   * Deletes the taskID if the current user is logged in.
+   * If the task is private, then only the owning user can delete it.
+   * @param taskId
+   */
   deleteTask: function (taskId) {
     var task = Tasks.findOne(taskId);
     if (task.private && task.owner !== Meteor.userId()) {
@@ -100,6 +151,12 @@ Meteor.methods({
 
     Tasks.remove(taskId);
   },
+  /**
+   * Allows logged in users to set tasks to checked.
+   * Private tasks can only be updated to their owner.
+   * @param taskId The task ID.
+   * @param setChecked The new value of setChecked.
+   */
   setChecked: function (taskId, setChecked) {
     var task = Tasks.findOne(taskId);
     if (task.private && task.owner !== Meteor.userId()) {
@@ -109,6 +166,11 @@ Meteor.methods({
 
     Tasks.update(taskId, { $set: { checked: setChecked} });
   },
+  /**
+   * Allows the owner of a task to set it as private status.
+   * @param taskId The task ID.
+   * @param setToPrivate The new privacy value.
+   */
   setPrivate: function (taskId, setToPrivate) {
     var task = Tasks.findOne(taskId);
 
